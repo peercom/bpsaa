1d05415ee48505157dbcafdc1e224c8fe59bc447
Holger von Ameln <holger.von.ameln@peercom.org>
2014-08-28 17:23:17 +0200

    fix routes

--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,7 @@ Aa::Application.routes.draw do
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-  root 'admin'
+  #root 'admin#index'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
