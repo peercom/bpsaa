6af75ad4c1e52d12e632896c922a8966df8e67fb
Holger von Ameln <holger.von.ameln@peercom.org>
2014-08-28 16:48:30 +0200

    unique articel numbers

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,5 +1,5 @@
 class Article < ActiveRecord::Base
    belongs_to :product
    validates_presence_of :article_number
-   
+   validates_uniqueness_of :article_number
 end
--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,7 @@ Aa::Application.routes.draw do
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-  # root 'welcome#index'
+  root 'admin'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
