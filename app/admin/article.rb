9b5c053334eebd3ceb304b37ec50900578215c51
Holger von Ameln <holger.von.ameln@peercom.org>
2014-09-02 16:45:19 +0200

    add transformer

--- app/admin/article.rb
+++ app/admin/article.rb
@@ -4,7 +4,7 @@ ActiveAdmin.register Article do
   # See permitted parameters documentation:
   # https://github.com/gregbell/active_admin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
   #
-  permit_params :product_id, :name, :description, :lamp, :length, :dimensions, :space_required, :efficiency, :suspension_distance, :ugr, :ugr1, :article_number, :eei, :degree, :fitting, :light_temperature
+  permit_params :product_id, :name, :description, :lamp, :transformer, :length, :dimensions, :space_required, :efficiency, :suspension_distance, :ugr, :ugr1, :article_number, :eei, :degree, :fitting, :light_temperature
   #
   # or
   #
@@ -34,6 +34,7 @@ ActiveAdmin.register Article do
           f.input :lamp, :label => "Leuchtmittel"
           f.input :length, :label => "Länge"
           f.input :eei
+          f.input :transformer, :label => "Netzgerät"
           f.input :efficiency, :label => "Wirkungsgrad"
           f.input :space_required, :label => "Platzbedarf"
           f.input :dimensions, :label => "Maße"
