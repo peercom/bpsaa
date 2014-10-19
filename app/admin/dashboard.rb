aa05e7ac0f94d6772c70ef8a48b88fe1a446fba4
Holger von Ameln <holger.von.ameln@peercom.org>
2014-10-19 19:04:31 +0200

    changes to dashboard

--- app/admin/dashboard.rb
+++ app/admin/dashboard.rb
@@ -5,16 +5,23 @@ ActiveAdmin.register_page "Dashboard" do
   content title: proc{ I18n.t("active_admin.dashboard") } do
 
     # app/admin/dashboard.rb
-    #section "Letzte Änderungen" do
-    #  table_for PaperTrail::Version.order('id desc').limit(20) do # Use PaperTrail::Version if this throws an error
-    #    column ("Item") { |v| v.item.try(:name) }
-    #    #column ("Item") { |v| link_to v.item, [:admin, v.item] } # Uncomment to display as link
-    #    column ("Vorgang") { |v| v.event }
-    #    column ("Typ") { |v| v.item_type.underscore.humanize }
-    #    column ("Geändert am") { |v| v.created_at.to_s :long }
-    #    column ("Nutzer") { |v| link_to AdminUser.find_by_email(v.whodunnit).email, [:admin, AdminUser.find_by_email(v.whodunnit)] }
-    #  end
-    #end
+    section "Letzte Änderungen" do
+      table_for PaperTrail::Version.order('id desc').limit(20) do # Use PaperTrail::Version if this throws an error
+        column ("Item") { |v| v.item.try(:name) }
+        #column ("Item") { |v| link_to v.item, [:admin, v.item] } # Uncomment to display as link
+        column ("Vorgang") { |v| v.event }
+        column ("Typ") { |v| v.item_type.underscore.humanize }
+        column ("Geändert am") { |v| v.created_at.to_s :long }
+        column ("Nutzer") {
+          |v|
+          unless AdminUser.find_by_email(v.whodunnit).blank?
+             link_to AdminUser.find_by_email(v.whodunnit).email, [:admin, AdminUser.find_by_email(v.whodunnit)]
+           else
+             "Unbekannt"
+           end
+        }
+      end
+    end
 
     # Here is an example of a simple dashboard with columns and panels.
     #
