bd9d65943e32b529fad2723cf84ccf02555f1e42
Holger von Ameln <holger.von.ameln@peercom.org>
2014-12-17 12:43:06 +0100

    fix a missing linked dir in deployment

--- config/deploy.rb
+++ config/deploy.rb
@@ -28,7 +28,7 @@ set :rvm_ruby_version, '2.0.0-p195'
 set :linked_files, %w{config/database.yml}
 
 # Default value for linked_dirs is []
-set :linked_dirs, %w{bin log tmp/pids tmp/cache tmp/sockets vendor/bundle public/system}
+set :linked_dirs, %w{bin log tmp/pids tmp/cache tmp/sockets vendor/bundle public/system public/uploads}
 
 # Default value for default_env is {}
 # set :default_env, { path: "/opt/ruby/bin:$PATH" }
